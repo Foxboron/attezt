de63389a78d58a8c2c1beba967afe2a9025fb57a
Morten Linderud <morten@linderud.pw>
2026-01-30 00:28:59 +0100

    transport: move TPM transports to a common package
    Then we can opaquely change between the test suite transport and the
    hardware TPM device transport.
    
    Signed-off-by: Morten Linderud <morten@linderud.pw>

---
+++ internal/transport/transport.go
@@ -0,0 +1,58 @@
+package transport
+
+import (
+	"fmt"
+	"io"
+	"testing"
+
+	"github.com/foxboron/attezt/internal/testutils"
+	ssim "github.com/google/go-tpm-tools/simulator"
+	"github.com/google/go-tpm/tpm2/transport"
+	"github.com/google/go-tpm/tpm2/transport/linuxtpm"
+	"github.com/google/go-tpm/tpmutil"
+)
+
+// TPM represents a connection to a TPM simulator.
+type TPM struct {
+	transport io.ReadWriteCloser
+}
+
+// Send implements the TPM interface.
+func (t *TPM) Send(input []byte) ([]byte, error) {
+	return tpmutil.RunCommandRaw(t.transport, input)
+}
+
+// Close implements the TPM interface.
+func (t *TPM) Close() error {
+	return t.transport.Close()
+}
+
+func OpenSimulator() (transport.TPMCloser, error) {
+	sim, err := ssim.GetWithFixedSeedInsecure(1234)
+	if err != nil {
+		return nil, err
+	}
+	return &TPM{
+		transport: sim,
+	}, nil
+}
+
+func GetTPM() (transport.TPMCloser, error) {
+	if testing.Testing() {
+		fmt.Println("initied testing")
+		rwc, err := OpenSimulator()
+		if err != nil {
+			return nil, err
+		}
+		// Ensure we set an EK Cert if we use the simulator
+		if err := testutils.SetEKCertificate(rwc); err != nil {
+			return nil, err
+		}
+		return rwc, err
+	}
+	rwc, err := linuxtpm.Open("/dev/tpmrm0")
+	if err != nil {
+		return nil, err
+	}
+	return rwc, nil
+}
